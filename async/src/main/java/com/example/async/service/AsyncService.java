b03f31afcc604f1a2176ca83d16cef7f775c51ec
pokabook <103029701+pokabook@users.noreply.github.com>
2022-05-22 15:15:33 +0900

    async

---
+++ async/src/main/java/com/example/async/service/AsyncService.java
@@ -0,0 +1,35 @@
+package com.example.async.service;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.scheduling.annotation.AsyncResult;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.Executor;
+
+@Service
+@Slf4j
+public class AsyncService {
+
+
+    @Async("async-thread")
+    public CompletableFuture run(){
+        return new AsyncResult(hello()).completable();
+    }
+    @Async
+    public String hello() {
+
+        for (int i = 0; i < 10; i++){
+            try {
+                Thread.sleep(2000);
+                log.info("thread sleep ...");
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }
+
+        return "async hello";
+    }
+}
