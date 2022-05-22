89cf4a18661a9ae7b87dc911b668683a4110c329
pokabook <103029701+pokabook@users.noreply.github.com>
2022-05-22 15:16:42 +0900

    Rest Template

---
+++ client/src/main/java/com/example/client/service/RestTemplateService.java
@@ -0,0 +1,63 @@
+package com.example.client.service;
+
+import com.example.client.dto.UserRequest;
+import com.example.client.dto.UserResponse;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import java.net.URI;
+
+@Service
+public class RestTemplateService {
+
+    //http://localhost:9090/api/server/hello
+    //response
+    public UserResponse hello(){
+        URI uri = UriComponentsBuilder.fromUriString("http://localhost:9090")
+                .path("/api/server/hello")
+                .queryParam("name", "aaaa")
+                .queryParam("age", 99)
+                .encode()
+                .build()
+                .toUri();
+
+        System.out.println(uri.toString());
+
+        RestTemplate restTemplate = new RestTemplate();
+        ResponseEntity<UserResponse> result = restTemplate.getForEntity(uri, UserResponse.class);//getForEntity Http에 get method를 뜻함
+
+        System.out.println(result.getStatusCode());
+        System.out.println(result.getBody());
+
+        return result.getBody();
+    }
+
+    public UserResponse post(){
+        // http://localhost:9090/api/server/user/{UserId}/name/{userName}
+
+        URI uri = UriComponentsBuilder
+                .fromUriString("http://localhost:9090")
+                .path("/api/server/user/{userId}/name/{userName}")
+                .encode()
+                .build()
+                .expand(100, "steve")
+                .toUri();
+        System.out.println(uri);
+
+        // http body -> object -> object mapper -> json -> rest template -> http body json
+        UserRequest req = new UserRequest();
+        req.setName("steve");
+        req.setAge(10);
+
+        RestTemplate restTemplate = new RestTemplate();
+        ResponseEntity<UserResponse> response = restTemplate.postForEntity(uri, req, UserResponse.class);
+
+        System.out.println(response.getStatusCode());
+        System.out.println(response.getHeaders());
+        System.out.println(response.getBody());
+
+        return response.getBody();
+    }
+}
